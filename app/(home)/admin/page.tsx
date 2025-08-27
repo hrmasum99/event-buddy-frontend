678ba24c4dcecdbbe4f6fba473fd595e99651089
Habibur Rahman Masum <hrmasum99.study@gmail.com>
2025-08-27 13:15:32 +0600

    Updated Fies

--- app/(home)/admin/page.tsx
+++ app/(home)/admin/page.tsx
@@ -1,26 +1,3 @@
-// import { Button } from "@/components/ui/button";
-// import CreateEvent from "@/components/ui/layouts/EventCreate";
-// import EventList from "@/components/ui/layouts/EventList";
-// import Link from "next/link";
-
-// export default function AdminDashboard() {
-//   return (
-//     <div className="px-10 sm:px-14 pt-2">
-//       <h5 className="text-xl text-[#242565] font-semibold">Admin Dashboard</h5>
-//       <div className="flex justify-between w-full mt-4 py-4">
-//         <h5 className="text-lg text-[#242565] font-medium">
-//           Events Management
-//         </h5>
-//         <CreateEvent />
-//       </div>
-
-//       <div>
-//         <EventList />
-//       </div>
-//     </div>
-//   );
-// }
-
 "use client";
 
 import { Button } from "@/components/ui/button";
