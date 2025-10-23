f6cc1b22790acd2d1d01ba20851736b2be9750ec
Habibur Rahman Masum <hrmasum99.study@gmail.com>
2025-10-23 19:05:03 +0600

    urls update

--- app/(home)/payments/do-payment/[id]/page.tsx
+++ app/(home)/payments/do-payment/[id]/page.tsx
@@ -2,15 +2,14 @@ import PaymentForm from "@/components/forms/PaymentForm";
 import ProtectedRoute from "@/components/ui/layouts/ProtectedRoute";
 
 interface PageProps {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }
 
-export default function PaymentPage({ params }: PageProps) {
+export default async function PaymentPage({ params }: PageProps) {
+  const { id } = await params;
   return (
     <ProtectedRoute requiredRole="user">
-      <PaymentForm bookingId={params.id} />
+      <PaymentForm bookingId={id} />
     </ProtectedRoute>
   );
 }
